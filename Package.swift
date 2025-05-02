ab5f84c860ec539398004a51719b42712e42ada3
Henrik Gustavii <henrik.gustavii@snappyshopper.co.uk>
2025-05-02 16:28:36 +0100

    Update PhoneNumberKit package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/marmelroy/PhoneNumberKit.git",
-            exact: "3.5.9"),
+            exact: "4.0.2"),
         .package(
             url: "https://github.com/checkout/checkout-risk-sdk-ios.git",
             exact: "2.0.1"),
